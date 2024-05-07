cdda7fa5878399e31b33e92029ebcd94c68c99e7
Matt Davis <matteius@gmail.com>
2024-05-07 11:14:10 -0400

    Debugging ioremap error

--- 4.4/isp/t31/tx-isp-module.c
+++ 4.4/isp/t31/tx-isp-module.c
@@ -10,27 +10,17 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/version.h>
-#include <misc/isp_mem_map.h>
-#include "txx-funcs.h"
 
 extern int tx_isp_init(void);
 extern void tx_isp_exit(void);
 
-static void __iomem *isp_base;
-
 static int __init tx_isp_module_init(void)
 {
-    isp_base = get_isp_base();
-    if (!isp_base) {
-        pr_err("Failed to initialize ISP\n");
-        return -EFAULT;
-    }
 	return tx_isp_init();
 }
 
 static void __exit tx_isp_module_exit(void)
 {
-    private_iounmap(isp_base);
 	tx_isp_exit();
 }
 
